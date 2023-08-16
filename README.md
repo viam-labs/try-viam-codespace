4603474b551e53a068dd5827dd96fe26812143ad
sean yu <55464069+hexbabe@users.noreply.github.com>
2023-08-16 17:27:00 +0000

    Updated readme with tutorial instructions and added basic drive.py file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # viam-python
 Try out the Python Viam SDK and use code to control a robot in our lab: app.viam.com/try
+
+# Get Started
+- Open the Codespace using this template on Github
+
+- Run `pip install viam-sdk` to grab the latest version of Viam's SDK
+
+- Go to https://app.viam.com/try and write down the address and secret key of the robot
+
+- Replace placeholders in code with said values
+
+- Run `python drive.py` and watch the robot move in the controls tab of the Try Viam app!
---
+++ drive.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from viam.components.base import Base
+from viam.robot.client import RobotClient
+from viam.rpc.dial import Credentials, DialOptions
+
+
+SECRET_FROM_VIAM_APP = 'dvk8u8rfyan00jehmpop0es5v66e620t417ofim49o7hoyct' # '' # todo: replace with secret in 'Security' tab on the Viam app
+ADDRESS_FROM_VIAM_APP = 'icy-feather-main.o7tr45zsem.viam.cloud' # '' # todo: replace with remote address in 'Control' tab on the Viam app
+
+async def connect():
+  creds = Credentials(
+    type='robot-location-secret',
+    payload=SECRET_FROM_VIAM_APP)
+  opts = RobotClient.Options(refresh_interval=0,
+                    dial_options=DialOptions(credentials=creds))
+  return await RobotClient.at_address(ADDRESS_FROM_VIAM_APP,
+                                      opts)
+
+
+async def moveInSquare(base):
+  for _ in range(4):
+    # moves the rover forward 500mm at 500mm/s
+    await base.move_straight(velocity=500, distance=500)
+    print("move straight")
+    # spins the rover 90 degrees at 100 degrees per second
+    await base.spin(velocity=100, angle=90)
+    print("spin 90 degrees")
+
+
+async def main():
+  robot = await connect()
+  base = Base.from_robot(robot, 'viam_base')
+  await moveInSquare(base)
+
+  # Don't forget to close the robot when you're done!
+  await robot.close()
+
+
+if __name__ == '__main__':
+  asyncio.run(main())
